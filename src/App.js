88da2c7466b206a69e935f7ec46c20c3a55f9200
Cat <404@errur.com>
2021-08-12 08:42:21 -0700

    Fixed and removed a few things

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import UserToDos from "./Components/UserToDos";
 // directions
 // for each user, display their name in h1 tags
 // underneath their name, add an h2 tag for "tasks"
-// display the user's to do list
+// when you click "tasks", display the user's to do list
 // you can get the list by making a call to: https://jsonplaceholder.typicode.com/users/:userId/todos
 //
 function App() {
+  const [users, setUsers] = useState([]);
+  
   useEffect(()=>{
     async function getUsers(){
       const response = await fetch('https://jsonplaceholder.typicode.com/users');
-      const usersFromApi = response.json();
+      const usersFromApi =  await response.json();
       setUsers(usersFromApi);
     }
     getUsers();
