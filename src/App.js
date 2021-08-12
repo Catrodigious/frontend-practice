33692f5687b8f2807f227c6681147a073221685e
Cat <404@errur.com>
2021-08-12 07:57:16 -0700

    Set up useEffect for getUsers.

--- src/App.js
+++ src/App.js
@@ -4,11 +4,19 @@ import UserToDos from "./Components/UserToDos";
 // https://jsonplaceholder.typicode.com/users
 // directions
 // for each user, display their name in h1 tags
-// underneath their name, have a second header that says "tasks"
+// underneath their name, add an h2 tag for "tasks"
 // display the user's to do list
 // you can get the list by making a call to: https://jsonplaceholder.typicode.com/users/:userId/todos
+//
 function App() {
-  
+  useEffect(()=>{
+    async function getUsers(){
+      const response = await fetch('https://jsonplaceholder.typicode.com/users');
+      const usersFromApi = response.json();
+      setUsers(usersFromApi);
+    }
+    getUsers();
+  })
   return (
     <div className="App">
     </div>
