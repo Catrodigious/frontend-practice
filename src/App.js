574fe06203858b02c7201f6e663cea924bc9ca37
Cat <404@errur.com>
2021-08-11 18:05:07 -0700

    Baseline commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, {useState, useEffect} from "react";
+import UserToDos from "./Components/UserToDos";
+
+// https://jsonplaceholder.typicode.com/users
+// directions
+// for each user, display their name in h1 tags
+// underneath their name, have a second header that says "tasks"
+// display the user's to do list
+// you can get the list by making a call to: https://jsonplaceholder.typicode.com/users/:userId/todos
+function App() {
+  
+  return (
+    <div className="App">
+    </div>
+  );
+}
+
+export default App;
